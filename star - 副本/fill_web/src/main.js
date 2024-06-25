69ea647fe9728d43b621b6aa80e7b989be83f564
specialweekss <1410029095@qq.com>
2024-06-26 01:39:02 +0800

    star

--- star - 副本/fill_web/src/main.js
+++ star - 副本/fill_web/src/main.js
@@ -2,6 +2,7 @@ import  { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus';
 import 'element-plus/theme-chalk/index.css';
+// 在main.js中引入组件
 import axios from 'axios'
 const app=createApp(App);
 window.Ip='http://'+process.env.VUE_APP_LOCAL_IP+':8090'
